d7dbc70facb426058d8f994c1fb6f84df5a1bd99
Jose L Quintanilla Blanco <44639383+JoseLQB@users.noreply.github.com>
2020-02-13 23:15:34 +0100

    Cambios menores

--- vistaMapa/scripts.js
+++ vistaMapa/scripts.js
@@ -3,31 +3,28 @@ $(document).ready(function () {
 });
 
 function movimientoMario(){
-    var juego1 = false;
-    var juego2 = false;
-    var juego3 = false;
-    var juego4 = false;
-    var juego5 = false;
+    
+    var juego= [true, true, true, true, true];
 
-    if(juego1 == false){
+    if(juego[0] == false){
         $("#mario").css({top: "22%"});
         $("#mario").animate({top: "10%"}, 2600);
         $("#mario").animate({left: "20%"}, 2100);
-    }else if(juego1 == true && juego2 == false){
+    }else if(juego[0] == true && juego[1] == false){
         $("#mario").css({left: "20%"});
         $("#mario").animate({top: "50%",}, 2100);
-    }else if(juego1 == true &&juego2 == true && juego3 == false){
+    }else if(juego[0] == true &&juego[1] == true && juego[2] == false){
         $("#mario").css({top: "50%", left: "20%"});
         $("#mario").animate({left: "50%",}, 2100);
-    }else if(juego1 == true &&juego2 == true && juego3 == true && juego4 == false){
+    }else if(juego[0] == true &&juego[1] == true && juego[2] == true && juego[3] == false){
         $("#mario").css({top: "50%", left: "50%"});
         setTimeout(function (){window.location.replace("../apuesta/index.html")}, 5300);
         $("#mario").animate({top: "10%",}, 2100);
-    }else if(juego1 == true &&juego2 == true && juego3 == true && juego4 == true&& juego5==false){
+    }else if(juego[0] == true &&juego[1] == true && juego[2] == true && juego[3] == true&& juego[4]==false){
         $("#mario").css({top: "10%", left: "50%"});
         $("#mario").animate({left: "70%",}, 2100);
-    }else if(juego1 == true &&juego2 == true && juego3 == true && juego4 == true && juego5==true){
+    }else if(juego[0] == true &&juego[1] == true && juego[2] == true && juego[3] == true && juego[4]==true){
         $("#mario").css({top: "10%", left: "70%"});
-        $("#mario").animate({left: "85%",}, 2100);
+        $("#mario").animate({left: "85%",}, 3100);
     }
 }
